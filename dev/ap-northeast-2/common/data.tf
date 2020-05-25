dce981153b8e525b704e679063dad80d4db08359
jjungeun <jungeun9729@gmail.com>
2020-05-25 18:02:43 +0900

    data

---
+++ dev/ap-northeast-2/common/data.tf
@@ -0,0 +1,16 @@
+data "aws_availability_zones" "available" {}
+
+locals {
+  zone_names = [
+    data.aws_availability_zone.a.name,
+    data.aws_availability_zone.c.name
+  ]
+}
+
+data "aws_availability_zone" "a" {
+  name = "ap-northeast-2a"
+}
+
+data "aws_availability_zone" "c" {
+  name = "ap-northeast-2c"
+}
