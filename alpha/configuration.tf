f26982e2d3396318a528e02dffb31c2964e02663
jjungeun <jungeun9729@gmail.com>
2020-06-03 00:04:00 +0900

    clustering 완료

---
+++ alpha/configuration.tf
@@ -0,0 +1,16 @@
+terraform{
+	required_version = "0.12.25"
+	backend "s3" {
+		region = "ap-northeast-2"
+		bucket = "kuberkuber-terraform-state"
+		key = "current/alpha/ap-northeast-2/common/terraform.tfstate"
+		encrypt = true
+		acl = "bucket-owner-full-control"
+		dynamodb_table = "kuberkuber-terraform-state-lock"
+	}
+}
+
+provider "aws" {
+	region = "ap-northeast-2"
+	version = "2.63.0"
+}
